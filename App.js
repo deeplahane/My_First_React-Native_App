af4e63c200537f2274f6954a199e7fb8983f31f2
deeplahane <deeplahane15@gmail.com>
2022-04-02 21:19:22 +0530

    second commit

--- App.js
+++ App.js
@@ -3,22 +3,33 @@ import { Text, StyleSheet, View } from "react-native";
 import CustomComponent from "./src/screens/CustomComponent";
 
 const App = () => {
+
+  const myName = "Deep Manohar Lahane";
+
   return (
     <View>
-      <Text style={styles.textStyle}> Hello World... App.js component </Text>
+      {/* <Text style={styles.textStyle}> Hello World... App.js component </Text> */}
+      <Text style={styles.textStyle} >Welcome to thapa technical channel</Text>
+      <Text style={styles.textSty} >We love react native and I am a subscriber of thapa technical channel</Text>
+      <Text>my name is {myName}.</Text>
+      {/* <CustomComponent/>
       <CustomComponent/>
       <CustomComponent/>
-      <CustomComponent/>
-      <CustomComponent/>
+      <CustomComponent/> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    color: "green",
-    marginTop: 200,
+    color: "blue",
+    fontWeight: 'bold',
+    fontSize: 40,
   },
+
+  textSty: {
+    fontSize: 30,
+  }
 });
 
 export default App;
--- src/screens/CustomComponent.js
+++ src/screens/CustomComponent.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import {Text,StyleSheet} from 'react-native';
+import React from "react";
+import { Text, StyleSheet } from "react-native";
 
-const CustomComponent = ()=>{
-    return <Text style={styles.textStyle}> hello World! this is my custom component</Text>;
+const CustomComponent = () => {
+  const myName = "Deep";
+  const myElement = <Text>Nice to meet you Guys!!!</Text>;
+
+  const myFullName = (fname,mname,lname) => {
+      return (
+        `my Full name is ${fname} ${mname} ${lname}`
+      );
+  }
+
+  return (
+    <view>
+    //   <Text style={styles.textStyle}>
+    //     {" "}
+    //     hello World! this is my custom component and my name is {myName}
+    //   </Text>
+    //   <Text> {myElement}</Text>
+    //   {"\n"}
+    //   <Text>{myFullName('Deep','Manohar','Lahane')}</Text>
+    </view> 
+  );
 };
 
 const styles = StyleSheet.create({
-    textStyle:{
-        color: "green",
-    },
+  textStyle: {
+    color: "green",
+  },
 });
 
-export default CustomComponent ;
+export default CustomComponent;
